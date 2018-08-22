a6d8547150b164b7744f7210e052902a16a592ff
adrianOstromecki <bloodrian@gmail.com>
2018-08-22 12:56:13 +0200

    Add toString() method to Race enum.

--- Race.java
+++ Race.java
@@ -1,3 +1,10 @@
 public enum Race{
     HUMAN, ELF, DWARF, ORC;
+
+    @Override
+    public String toString() {
+        String lowercaseName = this.name().toLowerCase();
+        String titlecaseName = lowercaseName.substring(0, 1).toUpperCase() + lowercaseName.substring(1);
+        return titlecaseName;
+    }
 }
