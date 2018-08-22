f00dd49774e66a460fd4465d1b660d37759a887b
adrianOstromecki <bloodrian@gmail.com>
2018-08-22 19:59:02 +0200

    Add inner builder static class to RaceConstraints class. Small generator fix.

--- RaceConstraints.java
+++ RaceConstraints.java
@@ -1,28 +1,31 @@
 import java.util.Random;
 
 public class RaceConstraints{
-    private int minHealth;
-    private int maxHealth;
-    private int minStrenght;
-    private int maxStrenght;
-    private int minIntelligence;
-    private int maxIntelligence;
-    private int minDexterity;
-    private int maxDexterity;
+    private final int minHealth;
+    private final int maxHealth;
+    private final int minStrenght;
+    private final int maxStrenght;
+    private final int minIntelligence;
+    private final int maxIntelligence;
+    private final int minDexterity;
+    private final int maxDexterity;
     private Random generator;
 
-    public RaceConstraints(int minHealth, int maxHealth, int minStrenght, int maxStrenght, 
-            int minIntelligence, int maxIntelligence, int minDexterity, int maxDexterity){
-        this.minHealth = minHealth;
-        this.maxHealth = maxHealth;
-        this.minStrenght = minStrenght;
-        this.maxStrenght = maxStrenght;
-        this.minIntelligence = minIntelligence;
-        this.maxIntelligence = maxIntelligence;
-        this.minDexterity = minDexterity;
-        this.maxDexterity = maxDexterity;
+    private RaceConstraints(RaceConstraintsBuilder builder){
+        this.minHealth = builder.minHealth;
+        this.maxHealth = builder.maxHealth;
+        this.minStrenght = builder.minStrenght;
+        this.maxStrenght = builder.maxStrenght;
+        this.minIntelligence = builder.minIntelligence;
+        this.maxIntelligence = builder.maxIntelligence;
+        this.minDexterity = builder.minDexterity;
+        this.maxDexterity = builder.maxDexterity;
 
         generator = new Random();
+    }
+
+    public String getTest(){
+        return minHealth + " " + maxHealth;
     }
 
     public int getRandomHealth(){
@@ -42,7 +45,73 @@ public class RaceConstraints{
     }
 
     private int getRandomNumber(int min, int max){
-        int value = generator.nextInt() % (max - min + 1) + min;
+        int value = generator.nextInt(max - min + 1) + min;
         return value;
+    }
+
+    public static class RaceConstraintsBuilder{
+        public int minHealth;
+        public int maxHealth;
+        private int minStrenght;
+        private int maxStrenght;
+        private int minIntelligence;
+        private int maxIntelligence;
+        private int minDexterity;
+        private int maxDexterity;
+
+        public RaceConstraintsBuilder(){
+            minHealth = 0;
+            maxHealth = 100;
+            minStrenght = 0;
+            maxStrenght = 20;
+            minIntelligence = 0;
+            maxIntelligence = 20;
+            minDexterity = 0;
+            maxDexterity = 20;
+        }
+
+        public RaceConstraintsBuilder minHealth(int minHealth){
+            this.minHealth = minHealth;
+            return this;
+        }
+
+        public RaceConstraintsBuilder maxHealth(int maxHealth){
+            this.maxHealth = maxHealth;
+            return this;
+        }
+
+        public RaceConstraintsBuilder minStrenght(int minStrenght){
+            this.minStrenght = minStrenght;
+            return this;
+        }
+
+        public RaceConstraintsBuilder maxStrenght(int maxStrenght){
+            this.maxStrenght = maxStrenght;
+            return this;
+        }
+
+        public RaceConstraintsBuilder minIntelligence(int minIntelligence){
+            this.minIntelligence = minIntelligence;
+            return this;
+        }
+
+        public RaceConstraintsBuilder maxIntelligence(int maxIntelligence){
+            this.maxIntelligence = maxIntelligence;
+            return this;
+        }
+
+        public RaceConstraintsBuilder minDexterity(int minDexterity){
+            this.minDexterity = minDexterity;
+            return this;
+        }
+
+        public RaceConstraintsBuilder maxDexterity(int maxDexterity){
+            this.maxDexterity = maxDexterity;
+            return this;
+        }
+
+        public RaceConstraints build(){
+            return new RaceConstraints(this);
+        }
     }
 }
