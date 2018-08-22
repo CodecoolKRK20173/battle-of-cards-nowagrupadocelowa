4af9809ad304b507cccb485c0a8e006819047c11
Mateusz <kaspazza@gmail.com>
2018-08-22 09:48:41 +0200

    created UserInterface

---
+++ UserInterface.java
@@ -0,0 +1,18 @@
+public class UserInterface{
+    public void print(String message){
+        System.out.println(message);
+    }
+
+    public String input(String question){
+        BufferedReader keyboardInput = null;
+        try {
+            keyboardInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(question);
+            String answer = keyboardInput.readLine();
+            return answer;
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+            return null;
+        }
+}
